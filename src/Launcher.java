a75df2052e3aee5549a51cc70937b19960726e72
fhheaney <fhheaney@wpi.edu>
2014-11-12 23:04:30 -0500

    Update Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,13 +2,15 @@
 public class Launcher {
 
 	public static void main(String[] args) {
-		//HelloWorld helloObj = new HelloWorld();
 		
+		//create new instance of a market
 		Trading market = new Trading();
+		//get all inputs from user
 		market.getInput();
+		//execute buy and sell orders
 		market.make_trades();
 		
-		//helloObj.sayHello();
+		
 		
 	
 	}
