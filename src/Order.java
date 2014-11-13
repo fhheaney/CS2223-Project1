3c2bb702bb07e9cd20e191bac563924591d47c23
fhheaney <fhheaney@wpi.edu>
2014-11-12 23:06:36 -0500

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -4,11 +4,13 @@ public abstract class Order implements Comparable<Order> {
 		private int price;
 		private int quantity;
 		
+		
 		public Order(int p, int q){
 			this.price = p;
 			this.quantity = q;
 		}
 		
+		//create comparable override for type order
 		@Override
 		public int compareTo(Order anotherOrder) {
 		    if (this.price > anotherOrder.getPrice())
@@ -21,18 +23,22 @@ public abstract class Order implements Comparable<Order> {
 		
 		public abstract void print();
 		
+		//returns price from current order
 		public int getPrice() {
 			return this.price;
 		}
 		
+		//sets price of current order
 		public void setPrice(int p) {
 			this.price = p;
 		}
 		
+		//returns qty from current order
 		public int getQuantity() {
 			return this.quantity;
 		}
 		
+		//sets qty of current order
 		public void setQuantity(int q) {
 			this.quantity = q;
 		} 
